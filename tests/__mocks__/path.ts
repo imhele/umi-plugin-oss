3b42578a85ead5031fa4b5639b7f380a55686c03
doubaozia <zhangdoujun@lilith.com>
2020-11-08 16:20:27 +0800

    remove old tests

--- tests/__mocks__/path.ts
+++
@@ -1,9 +0,0 @@
-export function join(...paths: string[]) {
-  return `/home/dist/${paths.slice(1).join('/')}`;
-}
-
-export function extname(name: string) {
-  return `.${name.split('.').reverse()[0]}`;
-}
-
-export default { join, extname };
